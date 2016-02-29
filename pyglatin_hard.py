011fd53a8b95f59077e05b30bb02e84c6554f0d5
Aaron Scruggs <aarondscruggs@gmail.com>
2016-02-29 13:40:38 -0800

    First hard draft

---
+++ pyglatin_hard.py
@@ -0,0 +1,23 @@
+# Hard mode
+# Complete all requirements of Easy Mode, as well as being able to convert many words in a single sentence.
+
+
+def translate_word(word):
+    if word[0] in "aeiou":
+        return word[1:] + "say"
+    else:
+        return "{}{}ay".format(word[1:], word[0])
+
+def convert(writing):
+    new_writing = []
+    for word in writing.split():
+        if word.isalpha():
+            new_writing.append(translate_word(word))
+        else:
+            new_writing.append(word)
+    return "".join(new_writing)
+
+
+writing = input("Please enter some text: ")
+
+print(convert(writing))
