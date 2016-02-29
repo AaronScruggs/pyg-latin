ffe5f1a2b106762cac0bf691e60ba27b23e96a29
Aaron Scruggs <aarondscruggs@gmail.com>
2016-02-29 13:18:41 -0800

    Basic word translation

---
+++ pyglatin.py
@@ -0,0 +1,15 @@
+# Easy mode
+# Create a program that when given a word, converts the word into it's pig latin version.
+
+
+def translate_word(word):
+    if word[0] in 'aeiou':
+        return word[1:] + 'say'
+    else:
+        return "{}{}ay".format(word[1:], word[0])
+
+
+word = input("Please enter a word: ").lower()
+
+print(translate_word(word))
+
