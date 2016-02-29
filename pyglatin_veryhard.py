be63d9dbe1fbb09bf6094c569636a5d8c4aeb7d6
Aaron Scruggs <aarondscruggs@gmail.com>
2016-02-29 14:13:44 -0800

    Basic functions done for very hard

--- pyglatin_veryhard.py
+++ pyglatin_veryhard.py
@@ -5,7 +5,26 @@ Complete all requirements of Hard Mode, as well as giving your program the abili
 """
 
 
+def translate_word(word):
+    if word[0] in "aeiou":
+        return word[1:] + "say"
+    else:
+        return "{}{}ay".format(word[1:], word[0])
+
+def convert(writing):
+    new_writing = []
+    for word in writing.split():
+        new_writing.append(translate_word(word.lower()))
+    return " ".join(new_writing)
+
 def back_to_english(writing):
-    pass
+    new_writing = []
+    for word in writing.split():
+        word = word.lower()
+        if word[-3:] == "say":
+            new_writing.append("_" + word[:-3])
+        else:
+            new_writing.append("{}{}".format(word[-3], word[:-3]))
+    return " ".join(new_writing)
 
 
