45c5ca5f0604bafb74e10cd5b9d58cf66c009a34
Aaron Scruggs <aarondscruggs@gmail.com>
2016-02-29 13:55:15 -0800

    Created very hard file

---
+++ pyglatin_veryhard.py
@@ -0,0 +1,11 @@
+"""
+Very hard mode
+Complete all requirements of Hard Mode, as well as giving your program the ability to read in a pig latin string
+ and output a plain english sentence. Replace first vowel characters with a "_".
+"""
+
+
+def back_to_english(writing):
+    pass
+
+
